775a3cd01c8c47619cdb81b74bbed4f148f8586f
Filippo <filippo.grotto@gmail.com>
2021-11-20 19:42:39 +0100

    Add pd gravity compensation (HW6)

---
+++ simulink/1_Gravity_Compensation/params.m
@@ -0,0 +1,30 @@
+clear;
+
+% URDF Parameters
+param.m1 = 1.3565;
+param.m2 = 1.1178;
+param.m3 = 0.0405;
+param.pi = 3.14;
+
+% From the urdf provided
+param.l0 = 0.4;
+param.l3 = 0.3/2;
+param.l1 = 0.4;
+param.l2 = 0.46;
+param.r1 = 0.02;
+param.b2 = 0.03;
+param.b3 = 0.01;
+param.g = 9.81;
+
+he = zeros(6,1);
+
+% Desired positions
+qd = [0.3, 0.45, 0.2];
+
+% PD Controller parameters
+Kp = diag([700 700 700]);
+Kd = diag([50 50 50]);
+
+
+
+
