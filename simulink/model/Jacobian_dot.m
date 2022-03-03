d564b2752101a2fa47ccb0291dd14292ab6c4738
Filippo <filippo.grotto@gmail.com>
2022-03-03 19:41:36 +0100

    Fix jacobian in model (no used)

--- simulink/model/Jacobian_dot.m
+++ simulink/model/Jacobian_dot.m
@@ -1,16 +1,17 @@
 function dot_J = Jacobian_dot(q,qd, param)
     t1 = q(1);
     d3 = q(3);
+    l1 = param.l1;
     l3 = param.l3;
     d_t1 = qd(1);
     d_d3 = qd(3);
 
-    dot_J =[[- d_d3*sin(t1) - d_t1*cos(t1)*(d3 + l3), -d_t1*sin(t1), -d_t1*sin(t1)]
-            [  d_d3*cos(t1) - d_t1*sin(t1)*(d3 + l3),  d_t1*cos(t1),  d_t1*cos(t1)]
-            [                                      0,             0,             0]
-            [                                      0,             0,             0]
-            [                                      0,             0,             0]
-            [                                      0,             0,             0]];
+    dot_J =[[- d_d3*sin(t1) - d_t1*cos(t1)*(d3 + l1 + l3), 0, -d_t1*sin(t1)]
+            [  d_d3*cos(t1) - d_t1*sin(t1)*(d3 + l1 + l3), 0,  d_t1*cos(t1)]
+            [                                           0, 0,             0]
+            [                                           0, 0,             0]
+            [                                           0, 0,             0]
+            [                                           0, 0,             0]];
 end
 
 
